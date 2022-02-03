a9fb47e54e38f3445f806f71b50e59f6410c7ca7
Rodrigo Salomon <rod_salomon@hotmail.com>
2022-02-02 21:47:04 -0300

    Adiciona Exercicio 4.1/2

--- 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/1D.introducao-javascript/E1/scripts.js
+++ 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/1D.introducao-javascript/E1/scripts.js
@@ -1,8 +1,13 @@
 const myName = "Rodrigo Salomon"
-const brithCity = "Rio de janeiro"
+const birthCity = "Rio de janeiro"
 let birthYear = 1985
 
 console.log(myName);
-console.log(brithCity);
+console.log(birthCity);
 console.log(birthYear);
 
+birthYear = 2030
+console.log(birthYear)
+
+birthCity = "São Paulo"
+console.log(birthCity)
---
+++ 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/1D.introducao-javascript/E2/scripts.js
@@ -0,0 +1,34 @@
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId)
+console.log(typeof isEnrolled)
+console.log(typeof patientInfo)
+console.log(typeof patientEmail)
+
+console.log(typeof patientAge)
+// // Exercício:
+// 1 - Crie uma costante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+
+// 2 - Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+
+// 3 - Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+
+const base = 5;
+let height = 8;
+
+const area = (base * height)
+
+console.log(area)
+
+// const perimeter = (base + height + area)
+
+// console.log(perimeter)
+
+let perimeter = 5+5+8+8
+console.log(perimeter)
