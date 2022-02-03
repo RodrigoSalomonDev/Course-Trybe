acd22972258d3adb5d9807143117898fe0d86196
Rodrigo Salomon <rod_salomon@hotmail.com>
2022-02-03 18:57:05 -0300

    Adiciona exercicio 4.2/FOR

--- 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/2D.array-e-loop-for/1E/scripts.js
+++ 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/2D.array-e-loop-for/1E/scripts.js
@@ -11,8 +11,19 @@
 // console.log(indexOfPortfolio);
 
 //Exercicio 3
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push('Contato')
-console.log(menu);
-menu.push('Page')
-console.log(menu);
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('Contato')
+// console.log(menu);
+// menu.push('Page')
+// console.log(menu);
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let index = 0; index < groceryList.length; index += 1){
+    // index = index.indexOf(groceryList)
+    console.log(groceryList[index])
+}
+    
+
+
+
