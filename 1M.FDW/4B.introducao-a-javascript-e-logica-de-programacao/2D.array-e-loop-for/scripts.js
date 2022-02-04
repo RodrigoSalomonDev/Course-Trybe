b1b5e185faa61963831fbd521d162d3c384ffe4e
Rodrigo Salomon <rod_salomon@hotmail.com>
2022-02-04 11:14:08 -0300

    Adiciona comentearios exercicio

--- 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/2D.array-e-loop-for/scripts.js
+++ 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/2D.array-e-loop-for/scripts.js
@@ -4,6 +4,14 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //     console.log(numeros)
 // }
 
+// false = 0
+// true = 1
+
+let somaNumeros = 0;
 for (i =0; i < numbers.length; i += 1){
-    console.log(numbers[i])
+    // somaNumeros = somaNumeros + numbers[i]
+    somaNumeros += numbers[i]
+    console.log(somaNumeros)
 }
+console.log(somaNumeros)
+
