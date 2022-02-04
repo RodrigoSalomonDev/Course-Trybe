e1264e4eff56d9c7577b633e7480e38c17159b64
Rodrigo Salomon <rod_salomon@hotmail.com>
2022-02-04 12:51:17 -0300

    Mensagem suspeita

---
+++ 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/2D.array-e-loop-for/teste.js
@@ -0,0 +1,15 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+let listaDeArrays = [patientId, isEnrolled, patientInfo, patientEmail]
+console.log("os tipos são:")
+for (let tipo of listaDeArrays){
+        console.log(typeof tipo)
+}
+
+console.log(typeof patientAge)
