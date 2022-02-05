d37e328c60cceeedc5835f2a15421a8585d8b9ee
Rodrigo Salomon <rod_salomon@hotmail.com>
2022-02-05 19:58:38 -0300

    Adiciona exercicio Switch/case

---
+++ 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/1D.introducao-javascript/E4/exercicioSwitchCase.js
@@ -0,0 +1,21 @@
+
+
+let trafficLight = "vermelho";
+
+  switch(trafficLight){
+    case "vermelho":
+      console.log("pare");
+      break;
+
+    case "amarelo":
+      console.log("atenção");
+      break;
+
+      case "vermelho":
+        console.log("siga");
+        break;
+
+    default:
+      console.log("valor não identificado");
+  }
+
