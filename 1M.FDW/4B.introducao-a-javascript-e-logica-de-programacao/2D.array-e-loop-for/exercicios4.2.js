efef44186fb879a04d3acaf94d1f438563b75a64
Rodrigo Salomon <rod_salomon@hotmail.com>
2022-02-08 12:22:15 -0300

    adicona exercicio 4.2

---
+++ 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/2D.array-e-loop-for/exercicios4.2.js
@@ -0,0 +1,20 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let i = 0; i < numbers.length; i+= 1) {
+//     console.log(numbers[i])
+// }
+// let soma = 0;
+// for (let i = 0; i < numbers.length; i+= 1) {
+//     // soma = soma + numbers[i]
+//     soma += numbers[i]
+// }
+//     console.log(soma)
+
+let soma = 0;
+for (let i = 0; i < numbers.length; i+= 1) {
+    // soma = soma + numbers[i]
+    soma += numbers[i]
+}
+    mediaAritimetica = soma / numbers.length;
+    console.log(mediaAritimetica)
+    console.log(numbers.length);
