ad1203375e0f89106eeb384bb40ecef866f2a677
Rodrigo Salomon <rod_salomon@hotmail.com>
2022-02-08 15:18:28 -0300

    Adiciona alteraçoes no exercicio 4.2

--- 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/2D.array-e-loop-for/exercicios4.2.js
+++ 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/2D.array-e-loop-for/exercicios4.2.js
@@ -1,4 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // for (let i = 0; i < numbers.length; i+= 1) {
 //     console.log(numbers[i])
@@ -10,11 +10,59 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 //     console.log(soma)
 
-let soma = 0;
-for (let i = 0; i < numbers.length; i+= 1) {
-    // soma = soma + numbers[i]
-    soma += numbers[i]
-}
-    mediaAritimetica = soma / numbers.length;
-    console.log(mediaAritimetica)
-    console.log(numbers.length);
+// let soma = 0;
+// for (let i = 0; i < numbers.length; i+= 1) {
+//     // soma = soma + numbers[i]
+//     soma += numbers[i]
+// }
+//     mediaAritimetica = soma / numbers.length;
+//     console.log(mediaAritimetica)
+//     console.log(numbers.length);
+
+// Exercicio 4
+// let soma = 0;
+// for (let i = 0; i < numbers.length; i+= 1) {
+//     // soma = soma + numbers[i]
+//     soma += numbers[i]
+// }
+//     mediaAritimetica = soma / numbers.length;
+
+//     if (mediaAritimetica > 20){
+//         console.log("Valor maior que 20.");
+//     } else {
+//         console.log("Valor menor ou igual a 20.");
+//     }
+    
+// Exercicio 5
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let numeroMaior = numbers[0];
+
+// for (let i = 0; i < numbers.length; i+= 1) {
+//        if (numeroMaior < numbers[i]){
+//         numeroMaior = numbers[i]
+//     }
+//     console.log(numeroMaior)
+//    }
+//     console.log(numeroMaior)
+
+
+// Exercicio 6
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let numeroImpar = 0;
+
+    for (let i = 0; i < numbers.length; i+= 1) {
+         if (numbers[i] % 2 !== 0){
+            numeroImpar += 1;
+        } 
+    }  
+    if (numeroImpar === 0){
+        console.log('Nenhum valor ímpar encontrado');
+    } else {
+        console.log(numeroImpar);
+    }  
+    
+
+
+
