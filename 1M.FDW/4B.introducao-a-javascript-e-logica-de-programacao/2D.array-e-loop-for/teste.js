b2a5a427758ce93acc82523e6c35f77af46bc467
Rodrigo Salomon <rod_salomon@hotmail.com>
2022-02-10 13:40:14 -0300

    Adiciona exercicios 4.2

--- 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/2D.array-e-loop-for/teste.js
+++ 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/2D.array-e-loop-for/teste.js
@@ -53,19 +53,25 @@
 //     console.log(nomes)
 //       }
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// o valor da variável higherNumber poderia ser qualquer um, desde que fosse menor que o menor número do array numbers. Caso atribuíssemos o valor 101 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número maior que 101 no array. Para resolver esse problema vamos iniciar a variavel com o primeiro valor do array. Dessa forma podemos pular a posição 0 e iniciar com index = 1
+// // o valor da variável higherNumber poderia ser qualquer um, desde que fosse menor que o menor número do array numbers. Caso atribuíssemos o valor 101 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número maior que 101 no array. Para resolver esse problema vamos iniciar a variavel com o primeiro valor do array. Dessa forma podemos pular a posição 0 e iniciar com index = 1
 
-let higherNumber = numbers[2];
+// let higherNumber = numbers[2];
 
-for (let index = 1; index < numbers.length; index += 1) {
-  if (numbers[index] > higherNumber) {
-    higherNumber = numbers[index];
-  }
+// for (let index = 1; index < numbers.length; index += 1) {
+//   if (numbers[index] > higherNumber) {
+//     higherNumber = numbers[index];
+//   }
+// }
+
+// console.log(higherNumber);
+
+
+function compareTrue(val1, val2) {
+ return val1 && val2;
 }
-
-console.log(higherNumber);
+console.log(compareTrue(true, true))
   
 
 
