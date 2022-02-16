59246796c14ef9f12a331df80fae135317307264
Rodrigo Salomon <rod_salomon@hotmail.com>
2022-02-16 19:25:57 -0300

    Adiciona exemplos

--- 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/4D.objetos-e-funcoes/objetos.js
+++ 1M.FDW/4B.introducao-a-javascript-e-logica-de-programacao/4D.objetos-e-funcoes/objetos.js
@@ -1,21 +1,98 @@
-let player = {
-    name: 'Marta',
-    lastName: 'Silva',
-    age: 34,
-    medals: {
-        golden: 2,
-        silver: 3,
-    }
-}
+//Exercícios Objetos 4
+// let player = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: {
+//         golden: 2,
+//         silver: 3,
+//     }
+// }
 
- player['fullName'] = player.name + ' ' + player.lastName;
+//  player['fullName'] = player.name + ' ' + player.lastName;
 
  
-console.log('A jogadora ' + player.fullName + ' tem ' + player.age + ' anos de idade.');
+// console.log('A jogadora ' + player.fullName + ' tem ' + player.age + ' anos de idade.');
 
-player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+// player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
 
-console.log('A jogadora ' + player.fullName + ' foi eleita a melhor do mundo ' + player.bestInTheWorld.length + ' vezes.');
+// console.log('A jogadora ' + player.fullName + ' foi eleita a melhor do mundo ' + player.bestInTheWorld.length + ' vezes.');
 
-console.log('A jogadora ' + player.fullName + ' possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+// console.log('A jogadora ' + player.fullName + ' possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
 
+	// let pizzas = {
+	// 	sabor: 'palmito',
+	// 	preco: '39.90',
+	// 	bordaDeCatupiry: true,
+	// 	};
+
+		// for (const key in pizzas) {
+		// 	console.log(key);
+		// 	}
+
+// 		for (let key in pizzas){
+// 			console.log(pizzas[key]);
+// 		}
+
+// let pizzasDoces = ['chocolate', 'banana', 'morango'];
+
+// for (let key in pizzasDoces){
+// 	console.log(key, pizzasDoces[key]);
+// }
+
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index in cars) {
+	
+//   console.log(index, cars[index]);
+// }
+
+// let car = {
+//   type: 'Fiat',
+//   model: '500',
+//   color: 'white',
+// };
+
+// for (let index in car) {
+//   console.log(index, car[index]);
+// }
+
+//FOR IN e FOR OF
+
+// let food = ['hamburguer', 'bife', 'acarajé'];
+
+// for (let position in food) {
+//   console.log(position);
+// };
+// //resultado: 0, 1, 2;
+
+// let food = ['hamburguer', 'bife', 'acarajé'];
+// for (let value of food) {
+//   console.log(value);
+// };
+// //resultado: hamburguer, bife, acarajé;
+
+
+	// let names = {
+	// 	person1: 'João',
+	// 	person2: 'Maria',
+	// 	person3: 'Jorge',
+	// };
+
+	// for (let key in names){
+	// 	console.log('Olá ' + names[key]);
+	// }
+
+		// let car = {
+		// 	model: 'A3 Sedan',
+		// 	manufacturer: 'Audi',
+		// 	year: 2020
+		// };
+
+		// for (let key in car){
+		// 	console.log(key, car[key]);
+		// }
+
+
+
+		
