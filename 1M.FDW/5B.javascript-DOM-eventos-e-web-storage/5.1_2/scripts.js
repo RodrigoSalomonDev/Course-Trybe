3302cb4af4da5c45d4589518bf3085cbf0d28e4c
Rodrigo Salomon <rod_salomon@hotmail.com>
2022-02-23 17:06:08 -0300

    Adciona alteraçoes do exercicio

---
+++ 1M.FDW/5B.javascript-DOM-eventos-e-web-storage/5.1_2/scripts.js
@@ -0,0 +1,18 @@
+let containerHeader = document.querySelector('#header-container');
+containerHeader.style.backgroundColor = '#3CB371';
+
+let urgente = document.querySelectorAll('h3');
+    urgente[0].style.backgroundColor = '#A020F0';
+    urgente[1].style.backgroundColor = '#A020F0';
+    urgente[2].style.backgroundColor = 'black';
+    urgente[3].style.backgroundColor = 'black';
+
+let emergency = document.querySelector('.emergency-tasks');
+    emergency.style.backgroundColor = '#FA8072';
+
+
+let noEmergency = document.querySelector('.no-emergency-tasks');
+    noEmergency.style.backgroundColor = 'gold';
+
+let footer = document.getElementById('footer-container')
+    footer.style.backgroundColor = '#0B5345'
