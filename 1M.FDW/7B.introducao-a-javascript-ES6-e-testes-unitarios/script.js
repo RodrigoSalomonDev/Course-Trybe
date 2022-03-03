f8160faa06b1f1325ce9362790ea27a521bddd6e
Rodrigo Salomon <rod_salomon@hotmail.com>
2022-03-03 16:23:31 -0300

    Adiciona modificacoes

--- 1M.FDW/7B.introducao-a-javascript-ES6-e-testes-unitarios/script.js
+++ 1M.FDW/7B.introducao-a-javascript-ES6-e-testes-unitarios/script.js
@@ -1,10 +1,42 @@
-let userEmail = 'email@test.com';
-function userInfo() {
-    let userEmail = 'maria@email.com';
+// let userEmail = 'email@test.com';
+// function userInfo() {
+//     let userEmail = 'maria@email.com';
   
-    // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
-    return console.log(userEmail);
-  }
-  userInfo();
+//     // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
+//     console.log(userEmail);
+//   }
+//   userInfo();
 
-  console.log(userEmail);
+//   console.log(userEmail);
+
+// if (true) {
+//     // inicio do escopo do if
+//     let userAge = '20';
+//     console.log(userAge); // 20
+//     // fim do escopo do if
+//   }
+//   console.log(userAge); // 20
+
+
+// const userName = 'Isabella';
+// let userName = 'Lucas';
+// console.log(userName); // Resultado: Lucas
+
+// const favoriteLanguage = 'Javascript';
+// favoriteLanguage = 'Python';
+// console.log(favoriteLanguage); // Erro
+
+// const favoriteTechnology = 'Machine learning';
+// favoriteTechnology = 'Facial recognition';
+// console.log(favoriteTechnology); // Facial recognition
+
+const userInfo = {
+    name: 'Cláudio',
+    id: '5489-2',
+    email: 'claudio@email.com',
+  };
+  
+  userInfo.sexo = 'masculino';
+  
+  console.log(userInfo); // { name: 'João', id: '5489-2', email: 'claudio@email.com' }
+
