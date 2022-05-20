930880b4552dfa66e3c593ea163246ac180055d0
Rodrigo Salomon <rod_salomon@hotmail.com>
2022-05-20 12:13:19 -0300

    Adiciona alterações exercicios 10.2

--- 2M.DFE/10B.introducao-a-react/10.2-exercicios-componentes-react/src/App.js
+++ 2M.DFE/10B.introducao-a-react/10.2-exercicios-componentes-react/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Card from "./Components/Card";
 import contents from "./data";
+import Greeting from "./Components/Exercícios/ExGreeting";
+import ImagemGato from "./Components/Exercícios Aula 10.2/imgs/catExercicio.jpg";
+import Image from "./Components/Exercícios/ExercicioGato";
 
 class App extends React.Component {
   sayHello() {
     return <h1>Olá, Deus!</h1>
+  }
+  gratidao() {
+    return <p>Obrigado pela benção de estar comigo.</p>
   }
 
   render(){
@@ -13,7 +19,10 @@ class App extends React.Component {
       <div>
         {
           this.sayHello()
-        } 
+        }
+        {
+          this.gratidao()
+        }
         <section>
           {
           contents.map((item) => {
@@ -24,13 +33,13 @@ class App extends React.Component {
           <Card modulo={2} titulo='Front-End'/>
           <Card modulo={3} titulo='Back-End'/>
           <Card modulo={4} titulo='Ciências da Computação'/> */}
+          
+          <Greeting name="Rodrigo" lastName="Salomon" />
         </section>
+        <Image source={ImagemGato} alternativeText="Cute cat staring" />
       </div>
     );
   }
 }
 
 export default App;
-
-
-//Parei em 01:19:00.
